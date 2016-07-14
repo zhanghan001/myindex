83065d7817ea87eed9cd257d849e2bc133857ef7
莫道 <modao@liuyuns-MacBook-Pro-2.local>
2016-07-14 17:12:59 +0800

    feat:some UI change

--- app/src/main/java/com/example/modao/moguindext/wedgit/mogujie_circle_imageview.java
+++ app/src/main/java/com/example/modao/moguindext/wedgit/mogujie_circle_imageview.java
@@ -3,8 +3,13 @@ package com.example.modao.moguindext.wedgit;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
@@ -44,23 +49,53 @@ public class Mogujie_circle_imageview extends ImageView {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        mBitmap = getRoundBitmap();
-        mBitmap=Bitmap.createBitmap(mBitmap)
-        float r;
-//        super.onDraw(canvas);
-        if (mBitmap.getWidth() > mBitmap.getHeight()) {
-            r = mBitmap.getHeight();
-        } else {
-            r = mBitmap.getWidth();
-        }
-//        RectF rect = new RectF(0, 0, r, r);
-        canvas.drawCircle(10,10,50,mPaint);
-
-    }
-
-    public Bitmap getRoundBitmap() {
         mBitmap = ((BitmapDrawable) getDrawable()).getBitmap();
-        return mBitmap;
+        Rect rect = new Rect(0, 0, mBitmap.getWidth(), mBitmap.getHeight());
+        Rect rect2 = new Rect(0, 0, getWidth(), getHeight());
+        mPaint.reset();
+
+        canvas.drawBitmap(getRoundBitmap(mBitmap), rect, rect2, mPaint);
+
     }
 
+    public Bitmap getRoundBitmap(Bitmap bitmap) {
+        float r;
+//        final  BitmapFactory.Options options =new BitmapFactory.Options();
+//        options.inJustDecodeBounds=true;
+//        BitmapFactory.decodeResource()
+        Bitmap output = Bitmap.createBitmap(getWidth(),
+                getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        Matrix matrix = new Matrix();
+        matrix.postScale(((float) getWidth())/ bitmap.getWidth(),((float) getWidth())/bitmap.getWidth()); //长和宽放大缩小的比例
+        Bitmap resizeBmp = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+
+        final Rect rect = new Rect(0, 0, getWidth(), getHeight());
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            r =getHeight();
+        } else {
+            r = getWidth();
+        }
+        canvas.drawCircle(r / 2, r / 2, r / 2, mPaint);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+        canvas.drawBitmap(resizeBmp,rect,rect,mPaint);
+        return output;
+    }
+    // TODO  use bitmap factory to scale bitmap
+//    public int calculateInSampleSize(BitmapFactory.Options op, int reqWidth,
+//                                     int reqheight) {
+//        int originalWidth = op.outWidth;
+//        int originalHeight = op.outHeight;
+//        int inSampleSize = 1;
+//        if (originalWidth > reqWidth || originalHeight > reqheight) {
+//            int halfWidth = originalWidth / 2;
+//            int halfHeight = originalHeight / 2;
+//            while ((halfWidth / inSampleSize > reqWidth)
+//                    &&(halfHeight / inSampleSize > reqheight)) {
+//                inSampleSize *= 2;
+//
+//            }
+//        }
+//        return inSampleSize;
+//    }
 }
